183b13df9884bbe394507516d0429a896179a4f0
ejnguyen1 <ejnguyen@ucsd.edu>
2025-05-13 16:47:41 -0700

    completed B10-13

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -94,10 +94,14 @@ function initFormHandler() {
 		recipes.push(recipeObject);
 		saveRecipesToStorage(recipes);
 	});
-	
 	// B10. TODO - Get a reference to the "Clear Local Storage" button
+	let clearButton = document.querySelector("button.danger");
 	// B11. TODO - Add a click event listener to clear local storage button
-	// Steps B12 & B13 will occur inside the event listener from step B11
-	// B12. TODO - Clear the local storage
-	// B13. TODO - Delete the contents of <main>
+	clearButton.addEventListener("click", () => {
+		// Steps B12 & B13 will occur inside the event listener from step B11
+		// B12. TODO - Clear the local storage
+		localStorage.clear();
+		// B13. TODO - Delete the contents of <main>
+		
+	});
 }
